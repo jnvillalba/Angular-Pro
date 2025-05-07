7e93a6c3e08e51ef0fdaa73e14c657625539639d
Joaquin Villalba <joaquinvillalba8@gmail.com>
2025-05-07 17:23:17 -0300

    fix: update express version and add @netlify/angular-runtime dependency; format angular.json and tsconfig.app.json

--- pokemon-ssr/server.ts
+++ pokemon-ssr/server.ts
@@ -17,6 +17,8 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
+  // Example Express Rest API endpoints
+  // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get(
     '**',
@@ -45,7 +47,14 @@ export function app(): express.Express {
   return server;
 }
 
-// Export the handler for Netlify Functions
-import serverless from 'serverless-http';
-const server = app();
-export const handler = serverless(server);
+function run(): void {
+  const port = process.env['PORT'] || 4000;
+
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+run();
