fc9a12291eed5413bc67a9c4a6b5b96f6eda48ed
Joaquin Villalba <joaquinvillalba8@gmail.com>
2025-05-10 14:33:14 -0300

    TanStack Query - Instalación

--- github-issues-tanstack/src/app/app.config.ts
+++ github-issues-tanstack/src/app/app.config.ts
@@ -1,16 +1,20 @@
 import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
-  provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import {
+  provideTanStackQuery,
+  QueryClient,
+  withDevtools,
+} from '@tanstack/angular-query-experimental';
 import { routes } from './app.routes';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideExperimentalZonelessChangeDetection(),
+    provideTanStackQuery(new QueryClient(), withDevtools()),
   ],
 };
